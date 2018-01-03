04089670c98de2a3e546eaa6628193f5af337bfd
yktaleb <taleb.yarik@gmail.com>
2018-01-03 17:14:40 +0200

    Added into servletContext attribute rootContext

--- CustomMVC/src/main/java/com/hw/config/WebConfig.java
+++ CustomMVC/src/main/java/com/hw/config/WebConfig.java
@@ -6,10 +6,8 @@ import com.hw.web.infrastructure.MyController;
 import com.hw.web.infrastructure.PizzaController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({RepositoryConfig.class, ServiceConfig.class})
 public class WebConfig {
 
     @Bean
--- CustomMVC/src/main/java/com/hw/web/infrastructure/SpringRootContextLoader.java
+++ CustomMVC/src/main/java/com/hw/web/infrastructure/SpringRootContextLoader.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,10 +16,25 @@ public class SpringRootContextLoader implements ServletContextListener {
         System.out.println(rootContexts);
 
         AnnotationConfigApplicationContext rootContext = new AnnotationConfigApplicationContext();
-//        rootContext.register();
+        for (Class<?> context : getConfigs(rootContexts)) {
+            rootContext.register(context);
+        }
+        rootContext.refresh();
 
         ServletContext servletContext = servletContext(servletContextEvent);
         servletContext.setAttribute("rootContext", rootContext);
+    }
+
+    private List<Class<?>> getConfigs(List<String> contexts) {
+        List<Class<?>> contextClasses = new ArrayList<>();
+        for (String context : contexts) {
+            try {
+                contextClasses.add(Class.forName(context));
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return contextClasses;
     }
 
     private ServletContext servletContext(ServletContextEvent servletContextEvent) {
